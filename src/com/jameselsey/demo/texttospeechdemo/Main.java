7d205f151d34f29832bcbafb332e2c7a6ba724ce
James Elsey <james.elsey@gmail.com>
2011-02-26 13:42:21 +0000

    Implemented TTS engine check and static TTS speech

--- src/com/jameselsey/demo/texttospeechdemo/Main.java
+++ src/com/jameselsey/demo/texttospeechdemo/Main.java
@@ -1,16 +1,95 @@
+/**
+ * Author : James Elsey
+ * Date : 26/Feb/2011
+ * Title : TextToSpeechDemo
+ * URL : Http://www.JamesElsey.co.uk
+ */
 package com.jameselsey.demo.texttospeechdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 
-public class Main extends Activity
+/**
+ * This class demonstrates checking for a TTS engine, and if one is
+ * available it will spit out some speak.
+ */
+public class Main extends Activity implements TextToSpeech.OnInitListener
 {
-    /** Called when the activity is first created. */
+    private TextToSpeech mTts;
+    // This code can be any value you want, its just a checksum.
+    private static final int MY_DATA_CHECK_CODE = 1234;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        System.out.println("hello");
+
+        // Fire off an intent to check if a TTS engine is installed
+        Intent checkIntent = new Intent();
+        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkIntent, MY_DATA_CHECK_CODE);
+
+    }
+
+    /**
+     * Executed when a new TTS is instantiated. Some static text is spoken via TTS here.
+     * @param i
+     */
+    public void onInit(int i)
+    {
+        mTts.speak("Hello folks, welcome to my little demo on Text To Speech.",
+                TextToSpeech.QUEUE_FLUSH,  // Drop all pending entries in the playback queue.
+                null);
+    }
+
+
+    /**
+     * This is the callback from the TTS engine check, if a TTS is installed we
+     * create a new TTS instance (which in turn calls onInit), if not then we will
+     * create an intent to go off and install a TTS engine
+     * @param requestCode int Request code returned from the check for TTS engine.
+     * @param resultCode int Result code returned from the check for TTS engine.
+     * @param data Intent Intent returned from the TTS check.
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == MY_DATA_CHECK_CODE)
+        {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS)
+            {
+                // success, create the TTS instance
+                mTts = new TextToSpeech(this, this);
+            }
+            else
+            {
+                // missing data, install it
+                Intent installIntent = new Intent();
+                installIntent.setAction(
+                        TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installIntent);
+            }
+        }
+    }
+
+    /**
+     * Be kind, once you've finished with the TTS engine, shut it down so other
+     * applications can use it without us interfering with it :)
+     */
+    @Override
+    public void onDestroy()
+    {
+        // Don't forget to shutdown!
+        if (mTts != null)
+        {
+            mTts.stop();
+            mTts.shutdown();
+        }
+        super.onDestroy();
     }
 }
